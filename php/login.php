2230aaa3b1d75a3bea1656fa840abe35548eba10
Luis Henrique Severo da Silva <luisseverodasilva17@gmail.com>
2023-12-12 21:06:54 -0300

    Atualizações Login 0.0.6

--- php/login.php
+++ php/login.php
@@ -1,1 +1,18 @@
+<?php
+session_start();
+include '../Crud/CrudUsuario.php';
 
+$crudUsuario = new CrudUsuario($db);
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    if ($crudUsuario->logar($email, $senha)) {
+        echo "Login bem-sucedido!";
+        header("Location: dash_adm.html");
+    } else {
+        echo "Login falhou. Verifique suas credenciais.";
+    }
+}
+?>
